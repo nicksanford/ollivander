fcc55f553e4743a29f22f536b6891db44c2dba11
Nick Sanford <nicholascsanford@gmail.com>
2016-04-09 16:11:32 -0400

    Update config/dev & config/test
    - Update configs to use default postgres user

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :ollivander, Ollivander.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "ollivander_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :ollivander, Ollivander.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "ollivander_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
